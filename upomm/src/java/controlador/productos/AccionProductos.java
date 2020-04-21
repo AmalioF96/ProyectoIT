1efe911178f9121abfb34fb18e0ea0f69070d8f2
markendos <markendos@gmail.com>
2020-04-21 21:55:11 +0200

    Principal con puntuaciones

--- upomm/src/java/controlador/productos/AccionProductos.java
+++ upomm/src/java/controlador/productos/AccionProductos.java
@@ -1,26 +1,50 @@
 package controlador.productos;
 
 import com.opensymphony.xwork2.ActionSupport;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import modelo.Productos;
+import modelo.Valoraciones;
 
 /**
  *
  * @author marwi
  */
 public class AccionProductos extends ActionSupport {
+
     private List<Productos> productos = null;
-    
+    private Map<Integer,Float> puntuaciones;
+
     public AccionProductos() {
+        this.puntuaciones = new HashMap();
     }
-    
+
     public String execute() throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     public String listar() {
-        List<Productos> listaProductos = modelo.DAO.ProductoDAO.listarProductos();
-        setProductos(listaProductos);
+        List<Productos> lp = modelo.DAO.ProductoDAO.listarProductos();
+        if (lp != null && !lp.isEmpty()) {
+            Iterator<Productos> it = lp.iterator();
+            while (it.hasNext()) {
+                int id = it.next().getIdProducto();
+                List<Valoraciones> lv = modelo.DAO.ProductoDAO.obtenerValoracionesProducto(id);
+                float puntuacion = 0;
+                if (lv != null && !lv.isEmpty()) {
+                    Iterator<Valoraciones> it2 = lv.iterator();
+                    while (it2.hasNext()) {
+                        puntuacion += it2.next().getPuntuacion();
+                    }
+                    puntuacion /= lv.size();
+                }
+                puntuaciones.put(id, puntuacion);
+                System.out.println("id=>" + id + "  puntuacion=>" + puntuacion);
+            }
+        }
+        setProductos(lp);
         return SUCCESS;
     }
 
@@ -31,7 +55,13 @@ public class AccionProductos extends ActionSupport {
     public void setProductos(List<Productos> productos) {
         this.productos = productos;
     }
-    
-    
-    
+
+    public Map getPuntuaciones() {
+        return puntuaciones;
+    }
+
+    public void setPuntuaciones(Map puntuaciones) {
+        this.puntuaciones = puntuaciones;
+    }
+
 }
