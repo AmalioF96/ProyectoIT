a577398ae85c491d78bc4c2a99c2d372c8305e42
markendos <markendos@gmail.com>
2020-04-26 18:04:15 +0200

    Mejoras carrito

--- upomm/src/java/controlador/ventas/AccionVenta.java
+++ upomm/src/java/controlador/ventas/AccionVenta.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador.ventas;
 
 import static com.opensymphony.xwork2.Action.ERROR;
@@ -19,7 +14,6 @@ import modelo.LineasDeCompra;
 import modelo.LineasDeCompraId;
 import modelo.Productos;
 import modelo.Usuarios;
-import modelo.Valoraciones;
 
 /**
  *
